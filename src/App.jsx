5771e859204aa2301ae7df610763edeb72ea5ed5
Murphybread <nargene@gmail.com>
2025-02-13 23:54:03 +0900

    style: change tabbutton

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,29 @@ function App() {
 
   return (
     <>
-      <Timer />
       <TabButton name="Home" />
       <TabButton name="Page1" />
       <button
         className="
-  px-6 
-  py-3 
-  bg-blue-500 
-  text-white 
-  rounded-lg 
-  shadow-md 
-  transition-all 
-  duration-300 
-  ease-in-out 
-  transform 
-  hover:-translate-y-4 
-  hover:shadow-xl
-  hover:delay-100
-"
+    px-6 
+    py-3 
+    bg-blue-500 
+    text-white 
+    rounded-lg 
+    shadow-md 
+    transition-all 
+    duration-300 
+    ease-in-out 
+    transform 
+    hover:-translate-y-4 
+    hover:shadow-xl
+    hover:delay-100
+  "
       >
         Hover Me
       </button>
       <ProjectContainer />
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Timer />
     </>
   );
 }
