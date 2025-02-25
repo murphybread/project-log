86a47caebbc220dcc660ebe815fb3ba1c652e0a8
Murphybread <nargene@gmail.com>
2025-02-25 23:09:04 +0900

    style: mix tailwind and general css

--- src/components/SideNavigation.jsx
+++ src/components/SideNavigation.jsx
@@ -5,12 +5,13 @@ export function SideNavigation() {
   return (
     <div className="side-navigation-container">
       <div className="side-navigation-title">Side Navigation Title</div>
-      <div className="side-navigation-items-container">
-        <div className="side-navigation-items">아이템1</div>
-        <div className="side-navigation-items">아이템2</div>
+      <div className="side-navigation-title-items-container">
+        <div className="side-navigation-items">타이틀 아이템1</div>
+        <div className="side-navigation-items">타이틀 아이템2</div>
       </div>
 
       <h3>Side Navigation item</h3>
+      <div className="side-navigation-items">아이템</div>
     </div>
   );
 }
