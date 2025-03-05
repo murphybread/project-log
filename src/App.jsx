ddc6096f71f400bc509149cae158cdb567a9c2a3
murphybread <nargene@gmail.com>
2025-03-05 17:47:37 +0900

    feat: create side navigation by mui

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,34 @@ import { ProjectContainer } from "@components/ProjectContainer.jsx";
 import { Timer } from "@components/Timer.jsx";
 import { SideNavigation } from "@components/SideNavigation.jsx";
 import { MuiComponents } from "@components/MuiComponents.jsx";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { SideNavigationMui } from "@components/SideNavigationMui.jsx";
+
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: "#3b82f6",
+    },
+  },
+  projectStatuses: {
+    success: {
+      label: "success",
+    },
+  },
+});
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <MuiComponents />
-      {/* <SideNavigation /> */}
-    </>
+    <ThemeProvider theme={theme}>
+      {/* <MuiComponents /> */}
+      <main style={{ display: "flex", gap: 30 }}>
+        <SideNavigation />
+        <SideNavigationMui />
+      </main>
+    </ThemeProvider>
   );
 }
 
